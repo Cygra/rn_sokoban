d5b8c9e4c3504fa66504a2f27f25907582f71434
Cygra <sjtuwbh@gmail.com>
2018-12-13 16:13:13 +0800

    update

--- helpers/readtxt.js
+++ helpers/readtxt.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 var gameA = fs.readFileSync('games/100Boxes.txt').toString().split('\n');
-var gameB = fs.readFileSync('games/696.txt').toString().split('\n');
+// var gameB = fs.readFileSync('games/696.txt').toString().split('\n');
 
 function getFromTxt(array) {
   var result = []
@@ -23,12 +23,16 @@ function getFromTxt(array) {
     title = result[i][result[i].length - 1].replace(' ;', '').replace('; ', '')
     result[i].pop()
 
-    max = findMaxLength(result[i])
+    max = result[i][result[i].reduce(
+      (prev, current, index, arr) =>
+        arr[prev].length > current.length ? prev : index,
+      0
+    )].length
 
     for (j in result[i]) {
       ij = result[i][j]
       len = ij.length
-      len < max && (result[i][j] = ij + spaces(max - len))
+      len < max && (result[i][j] = ij.padEnd(max))
     }
     max = 0
 
@@ -38,30 +42,9 @@ function getFromTxt(array) {
   return obj
 }
 
-function spaces(len) {
-  var temp = ''
-  for (var i = 0; i < len; i++) {
-    temp = temp + ' '
-  }
-  return temp
-}
-
-function findMaxLength(arr) {
-  var _max = 0
-  var _len = 0
-
-  arr.forEach(i => {
-    _len = i.length
-    if (_len > _max) _max = _len
-  });
-  return _max
-}
-
-fs.writeFile('games.js', `export const games = JSON.parse('${JSON.stringify({
-  ...getFromTxt(gameA),
-  ...getFromTxt(gameB),
-})}')`, {
-  flag: 'w',
-}, function(err) {
-  console.log(err)
-})
+fs.writeFile(
+  'games.js',
+  `export const games = JSON.parse('${JSON.stringify({ ...getFromTxt(gameA) })}')`,
+  { flag: 'w' },
+  err =>  console.log(err)
+)
